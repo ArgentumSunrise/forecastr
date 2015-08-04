01953df415c5851e9096ba74377a20baf4aa21d7
Colter Giem <coltergiem@gmail.com>
2015-08-03 23:20:14 -0600

    Added display icons and stat rounding

--- js/main.js
+++ js/main.js
@@ -34,22 +34,28 @@ function cityWeather(location, woeid, unitType) {
 
             if (location == "The Sun" || location == 'the sun' || location == 'sun' || location == 'Sun') {
                 cityText = '<h1 id="city">The Sun</h1>';
-                weather.temp = '&#x7E;' + '5800';
+                weather.temp = '5800';
                 weather.units.temp = 'K';
-                weather.currently = 'Fiery Inferno of Death';
+                weather.currently = 'Horrible Death';
                 weather.humidity = 0;
                 weather.wind.direction = 'E';
                 weather.wind.speed = 900;
                 weather.units.speed = 'km/s';
+                weather.units.distance = '';
+                weather.visibility = 'Fire';
                 $('#metric').hide();
             }
             header = cityText;
             header += '<h1 id="temp">' + weather.temp + '&deg;' + weather.units.temp;
             $('#header').html(header);
 
+            weather.wind.speed = Math.round(weather.wind.speed * 10) / 10;
+            weather.visibility = Math.round(weather.visibility * 10) / 10;
+
             humidwind = '<h1 id="weather-text">' + weather.currently + '</h1>';
-            humidwind += '<h1 id="humidity">Hum ' + weather.humidity + '%' + '</h1>';
-            humidwind += '<h1 id="wind">' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed.toUpperCase() + ' ' + '</h1>';
+            humidwind += '<h1><img class="display-icon" src="imgs/display-icons/humidity.png">' + weather.humidity + '%' + '</h1>';
+            humidwind += '<h1><img class="display-icon" src="imgs/display-icons/wind.png">' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed.toUpperCase() + '</h1>';
+            humidwind += '<h1><img class="display-icon" src="imgs/display-icons/visibility.png">' + weather.visibility + ' ' + weather.units.distance.toUpperCase() + '</h1>';
             $('#humid-wind').html(humidwind);
             $('#second-row').append('<div id="weather"><i class="fa fa-sun-o"></i></div>');
             $('#second-row').show();
