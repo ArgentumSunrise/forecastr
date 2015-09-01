b8590ad302b25b4eb8179a7e8c3a60c7088390d4
Colter Giem <coltergiem@gmail.com>
2015-08-31 21:09:35 -0600

    Did a bunch of stuff

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@
 // I'll get this functional eventually... sigh...
 
 function cityWeather(location, woeid, unitType) {
+    var forecasts = {};
     $.simpleWeather({
         location: location,
         woeid: woeid,
@@ -19,10 +20,10 @@ function cityWeather(location, woeid, unitType) {
             var reg = "";
             var temp = "";
             var speed = "";
-            var cityText = "";
             var header = "";
             var humidwind = "";
-            var specialCase = false;
+            var cityText = "";
+            var reg = "";
 
             weather.humidity = (weather.humidity ? weather.humidity : 0);
             weather.visibility = (weather.visibility ? weather.visibility : 0);
@@ -53,8 +54,8 @@ function cityWeather(location, woeid, unitType) {
                 weather.visibility = '56.7';
                 $('#metric').hide();
             }
-            header = cityText;
-            header += '<h1 id="temp">' + weather.temp + '&deg;' + weather.units.temp;
+
+            header += cityText + '<h1 id="temp">' + weather.temp + '&deg;' + weather.units.temp;
             $('#header').html(header);
 
             weather.wind.speed = Math.round(weather.wind.speed * 10) / 10;
@@ -70,7 +71,32 @@ function cityWeather(location, woeid, unitType) {
             // $('#loading').hide();
         },
         error: function (error) {
-            $('weatherDisplay').html('<h1 id="error"> We' + '&rsquo;' + 're sorry, but there appears to be an error. We cannot currently display your weather.</h1>');
+            $('weatherDisplay').html('<h1> We' + '&rsquo;' + 're sorry, but there appears to be an error. We cannot currently display your weather.</h1>');
         }
     });
+};
+
+function cityForecast(location, woeid, unitType) {
+    var forecasts = {};
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit: unitType,
+        success: function (weather) {
+            for (var i = 0; i < 5; i++) {
+                forecastNums.push({
+                    day: weather.forecast[i].day,
+                    code: weather.forecast[i].code,
+                    high: weather.forecast[i].high,
+                    low: weather.forecast[i].low,
+                });
+                if (forecastNums.length >= 6) {
+                    forecastNums.shift();
+                }
+            }
+        },
+        error: function (error) {
+            $('weatherDisplay').html('<h1> We' + '&rsquo;' + 're sorry, but there appears to be an error. We cannot currently display your weather.</h1>');
+        }
+    })
 };
